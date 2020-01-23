2bd170a39bddfebbbeb82f427302b99ac56de063
Shadrack <shadrackkimutai@gmail.com>
2020-01-23 09:17:04 +0300

    Fixing Migrations
    Fixed Migrations. Now we are on track

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -14,11 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->default('user.jpg');
+             $table->text('expertice')->nullable();
+        $table->integer('rating')->nullable();
+        $table->string('availability')->default('full time');
+        $table->string('area')->nullable();
+        $table->string('location')->nullable();
+        $table->string('contacts')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
