78db3c5c11478324a76df8da2df8239ee2b51ec0
Shadrack Kimutai <shadrackkimutai@gmail.com>
2020-02-06 09:24:28 +0300

    handled some  bs
    handled some  bs

--- resources/views/entrepreneurs.blade.php
+++ resources/views/entrepreneurs.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-   
+   <style>
+  .locationMap {
+    height: 300px;
+  }
+</style>
 <div class="container">
        <div style="height: 100px; width:100%" class="row"></div>
     <div class="row justify-content-center">
@@ -26,6 +30,9 @@
                 <td>        {{ $entre->contacts }}         </td>
                
                 <td>         {{ $entre->rating}}        </td>
+                <td>    <a type="button" onclick="" ="open" class="btn btn-primary" data-toggle="modal" data-target="#myModal" data-lngltd="{{ $entre->location }}">
+    Locate
+  </button></td>
             </tr>
             @endforeach
 </table>
@@ -33,5 +40,43 @@
     </div>
 </div>
 </div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="mapModal">
+          Location
+        </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">
+            &times;
+          </span>
+        </button>
+      </div>
+      <div class="modal-body">
+       <gmap-map
+          class="locationMap"
+          :center="{lat:10, lng:10}"
+          :zoom="1"
+        ></gmap-map>
+      </div>
+    </div>
+  </div>
+</div>
 
+<script>
+   methods: {
+initMapModal(event){
+  event.preventDefault();
+  setTimeout(this.mapResize, 200);
+},
+
+mapResize(){
+  Vue.$gmapDefaultResizeBus.$emit('resize');
+},
+
+}
+
+
+</script>
 @endsection
