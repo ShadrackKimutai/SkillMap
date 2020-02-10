00d4581bcfc39730e8739991f32f95ffe44393fd
Shadrack Kimutai <shadrackkimutai@gmail.com>
2020-02-10 16:17:31 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar')->default('user.jpg');
+            $table->string('avatar')->default('user.png');
              $table->text('expertice')->nullable();
             $table->text('icon')->nullable();
 
